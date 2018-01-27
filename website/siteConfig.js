0e9294f41cacecabf009d15fdd50693d80e7b2c7
Brent Vatne <brentvatne@gmail.com>
2018-01-27 13:38:57 -0800

    Remove unused files

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const siteConfig = {
   projectName: 'react-navigation',
   headerLinks: [
     {doc: 'getting-started', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'api', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
