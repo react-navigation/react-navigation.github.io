340e87d1cf2c59545cee4671b6b48dc5f6e412d7
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2020-01-08 14:51:20 +0100

    revert adding NavigationNativeContainer (#603)

--- website/static/examples/next/native-stack-with-options.js
+++ website/static/examples/next/native-stack-with-options.js
@@ -37,40 +37,46 @@ function Settings({ navigation }) {
 
 const Stack = createNativeStackNavigator();
 
+function MyStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        headerTintColor: 'white',
+        headerStyle: { backgroundColor: 'tomato' },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Awesome app',
+        }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          title: 'My profile',
+        }}
+      />
+      <Stack.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          gestureEnabled: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   enableScreens();
   return (
     <NavigationNativeContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false,
-          headerTintColor: 'white',
-          headerStyle: { backgroundColor: 'tomato' },
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: 'Awesome app',
-          }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            title: 'My profile',
-          }}
-        />
-        <Stack.Screen
-          name="Settings"
-          component={Settings}
-          options={{
-            gestureEnabled: false,
-          }}
-        />
-      </Stack.Navigator>
+      <MyStack />
     </NavigationNativeContainer>
   );
 }
