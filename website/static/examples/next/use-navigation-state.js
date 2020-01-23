7a52c301216e4a34f06f686c9b8b4e86ec523d18
WoLewicki <wojciech.lewicki@swmansion.com>
2020-01-23 16:11:22 +0100

    fix: change snack

--- website/static/examples/next/use-navigation-state.js
+++ website/static/examples/next/use-navigation-state.js
@@ -1,71 +1,89 @@
 import * as React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Title, Button } from 'react-native-paper';
-import { Feather } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button, View, Text } from 'react-native';
 import {
   NavigationNativeContainer,
+  useRoute,
   useNavigationState,
 } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const BottomTabs = createBottomTabNavigator();
+function useIsFirstRouteInParent() {
+  const route = useRoute();
+  const isFirstRouteInParent = useNavigationState(
+    state => state.routes[0].key === route.key
+  );
 
-function TabScreen(props) {
-  const routesLength = useNavigationState(state => state.routes.length);
+  return isFirstRouteInParent;
+}
 
+function getPreviousRouteName() {
+  return useNavigationState(state =>
+    state.routes[state.index - 1]?.name
+      ? state.routes[state.index - 1].name
+      : 'None'
+  );
+}
+
+function HomeScreen({ navigation }) {
+  const isFirstRoute = useIsFirstRouteInParent();
+  const previousRouteName = getPreviousRouteName();
   return (
-    <View style={styles.container}>
-      <Title>Tab {props.i}</Title>
-      <Text>Number of routes: {routesLength}</Text>
-      <Button onPress={() => props.setTabs(tabs => [...tabs, tabs.length])}>
-        Add a tab
-      </Button>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>It is {isFirstRoute ? '' : 'not '}first route in navigator</Text>
+      <Text>Previous route name: {previousRouteName}</Text>
+
       <Button
-        onPress={() =>
-          props.setTabs(tabs => (tabs.length > 1 ? tabs.slice(0, -1) : tabs))
-        }
-      >
-        Remove a tab
-      </Button>
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
     </View>
   );
 }
 
-function BottomTabsScreen() {
-  const [tabs, setTabs] = React.useState([0, 1]);
-
+function ProfileScreen({ navigation }) {
+  const isFirstRoute = useIsFirstRouteInParent();
+  const previousRouteName = getPreviousRouteName();
   return (
-    <BottomTabs.Navigator>
-      {tabs.map(i => (
-        <BottomTabs.Screen
-          key={i}
-          name={`tab-${i}`}
-          options={{
-            title: `Tab ${i}`,
-            tabBarIcon: ({ color, size }) => (
-              <Feather name="octagon" color={color} size={size} />
-            ),
-          }}
-        >
-          {() => <TabScreen i={i} setTabs={setTabs} />}
-        </BottomTabs.Screen>
-      ))}
-    </BottomTabs.Navigator>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>It is {isFirstRoute ? '' : 'not '}first route in navigator</Text>
+      <Text>Previous route name: {previousRouteName}</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  const isFirstRoute = useIsFirstRouteInParent();
+  const previousRouteName = getPreviousRouteName();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>It is {isFirstRoute ? '' : 'not '}first route in navigator</Text>
+      <Text>Previous route name: {previousRouteName}</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
   );
 }
 
 export default function App() {
   return (
     <NavigationNativeContainer>
-      <BottomTabsScreen />
+      <MyStack />
     </NavigationNativeContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
