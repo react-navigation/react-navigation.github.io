23899163fcad28b84cbb405c4ea901aebec516f2
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-02-05 20:42:39 +0100

    Fix edit url

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -33,7 +33,7 @@ class Users extends React.Component {
             <div className="logos">{showcase}</div>
             <p>Are you using this project?</p>
             <a
-              href="https://github.com/facebook/docusaurus/edit/master/website/siteConfig.js"
+              href="https://github.com/react-navigation/react-navigation.github.io/edit/source/website/siteConfig.js"
               className="button">
               Add your company
             </a>
