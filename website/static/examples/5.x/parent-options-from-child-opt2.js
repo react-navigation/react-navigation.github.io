45d0328a73adfea5b44b491cdc4b1840adcc101e
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-02-09 01:01:08 +0100

    Also consider route.params.screen to determine active screen

--- website/static/examples/5.x/parent-options-from-child-opt2.js
+++ website/static/examples/5.x/parent-options-from-child-opt2.js
@@ -9,9 +9,9 @@ function getHeaderTitle(route) {
   const routeName = route.state
     ? // Get the currently active route name in the tab navigator
       route.state.routes[route.state.index].name
-    : // If state doesn't exist, we need to default to the initial screen
+    : // If state doesn't exist, we need to default to `screen` param if available, or the initial screen
       // In our case, it's "Feed" as that's the first screen inside the navigator
-      'Feed';
+      route.params?.screen || 'Feed';
 
   switch (routeName) {
     case 'Feed':
