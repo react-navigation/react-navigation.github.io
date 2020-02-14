f654a43a529f18c85f13545e6c70763fc0e6f779
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-02-14 23:32:23 +0100

    Render splash outside navigation

--- website/static/examples/5.x/auth-flow.js
+++ website/static/examples/5.x/auth-flow.js
@@ -125,21 +125,24 @@ export default function App({ navigation }) {
     []
   );
 
+  if (state.isLoading) {
+    // We haven't finished checking for the token yet
+    return <SplashScreen />;
+  }
+
   return (
     <AuthContext.Provider value={authContext}>
       <NavigationContainer>
         <Stack.Navigator>
-          {state.isLoading ? (
-            // We haven't finished checking for the token yet
-            <Stack.Screen name="Splash" component={SplashScreen} />
-          ) : state.userToken == null ? (
+          {state.userToken == null ? (
             // No token found, user isn't signed in
             <Stack.Screen
               name="SignIn"
               component={SignInScreen}
               options={{
                 title: 'Sign in',
-            // When logging out, a pop animation feels intuitive
+                // When logging out, a pop animation feels intuitive
+                // You can remove this if you want the default 'push' animation
                 animationTypeForReplace: state.isSignout ? 'pop' : 'push',
               }}
             />
