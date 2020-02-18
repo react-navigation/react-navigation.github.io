35b500688d75883fd6f0c609c3e054786152929d
Erivelton Elias <eriveltonelias@icloud.com>
2020-02-18 08:45:45 -0300

    remove .docusaurus

--- website/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/eriveltonelias/Development/github/react-navigation.github.io/website/src/css/custom.css"),
-];
