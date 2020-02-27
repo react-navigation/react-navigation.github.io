769d8349a38dfa0bc274f01e162887a041a703ca
Brent Vatne <brentvatne@gmail.com>
2020-02-27 11:00:55 -0800

    Revert "Revert "Revert "Fix Snack links"""
    This reverts commit a8c5a23d8575b759f01dc958617fc27819ad4aec.

--- static/js/snack.js
+++ static/js/snack.js
@@ -1,5 +1,3 @@
-// NOTE(brentvatne): the package:version meta tag isn't available when this code
-// first executes....
 const DEFAULT_PLATFORM = 'android';
 const DEPS_VERSIONS = {
   '4': [
@@ -25,12 +23,16 @@ const DEPS_VERSIONS = {
   next: [],
 };
 
-function getVersion() {
-  if (window.__reactNavigationVersion) {
-    return window.__reactNavigationVersion;
+function getVersion(){
+  const navBarItems = document.getElementsByClassName('navbar__items');
+  if(navBarItems[0] && navBarItems[0].children[2] && navBarItems[0].children[2].textContent){
+    return navBarItems[0].children[2].textContent;
   }
+  return 'next';
 }
-
+// todo: should get the version somewhere, maybe within the page html,
+// and match the appropriate version of react-navigation/stack/tabs/drawer
+// based on that
 function getSnackUrl(options) {
   let currentVersion = getVersion();
   let label = options.label || document.title;
@@ -57,20 +59,24 @@ function getSnackUrl(options) {
   }
 }
 
-function findNearestCodeBlock(node) {
+function findNearestPreElement(node) {
   let nextElement = node.nextElementSibling;
   if (!nextElement && node.parentElement.tagName === 'P') {
     nextElement = node.parentElement.nextElementSibling;
   }
 
   while (nextElement) {
+    if(nextElement.tagName === 'DIV' && nextElement.className.includes('mdxCodeBlock_node_modules-@docusaurus-theme-classic-src-theme-MDXComponents-')){
+      nextElement = nextElement.firstChild;
+    }
     if (
-      nextElement.tagName === 'DIV' &&
-      nextElement.className.includes(
-        'mdxCodeBlock_node_modules-@docusaurus-theme-classic-src-theme-MDXComponents-'
-      )
+      nextElement.tagName === 'PRE' &&
+      nextElement.children[1] &&
+      nextElement.children[1].tagName === 'CODE'
     ) {
-      return nextElement;
+      return nextElement.children[1];
+    } else {
+      nextElement = nextElement.nextElementSibling;
     }
   }
 }
@@ -86,18 +92,19 @@ function appendSnackLink() {
   }
 
   samples.forEach(samp => {
-    let codeBlock = findNearestCodeBlock(samp);
+    let pre = findNearestPreElement(samp);
 
-    if (!codeBlock) {
+    if (!pre) {
       console.log(
-        `Code block not found for <samp> element ${samp.innerText}`
+        `<pre> tag with <code> child not found for <samp> element ${samp.innerText}`
       );
       return;
     }
 
-    let code = codeBlock.innerText;
+    let code = pre.innerText;
     let label = samp.innerText;
     let templateId = samp.getAttribute('id');
+
 
     let link = document.createElement('a');
     link.className = 'snack-sample-link';
@@ -110,6 +117,7 @@ function appendSnackLink() {
       link.innerHTML = `Try this example on Snack ${openIcon}`;
     }
 
+
     // Add the href and append the link element if we have some code
     let href = getSnackUrl({ code, templateId, label });
 
@@ -119,7 +127,7 @@ function appendSnackLink() {
 
     link.href = href;
 
-    codeBlock.insertAdjacentElement('afterend', link);
+    pre.insertAdjacentElement('afterend', link);
 
     // Don't try to add the link more than once!
     samp.remove();
