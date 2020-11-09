3a6de269efb19fc032a1abc7ab41022fd4cc521d
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-11-09 21:57:33 +0100

    Show resulting action in playground

--- src/components/LinkingTester.js
+++ src/components/LinkingTester.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { getStateFromPath } from '@react-navigation/core';
+import { getActionFromState, getStateFromPath } from '@react-navigation/core';
 import escape from 'escape-html';
 import Editor from 'react-simple-code-editor';
 import Highlight, { defaultProps } from 'prism-react-renderer';
@@ -9,6 +9,24 @@ import ThemeContext from '@theme/ThemeContext';
 import RouteMap from './RouteMap';
 
 const parse = (value) => eval(`(function() { return ${value}; }())`);
+
+function Code({ code, theme, language }) {
+  return (
+    <Highlight {...defaultProps} code={code} theme={theme} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, ...styles.json }}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+}
 
 export default function LinkingTester() {
   const { isDarkTheme } = React.useContext(ThemeContext);
@@ -38,12 +56,13 @@ export default function LinkingTester() {
 
   const [path, setPath] = React.useState('/user/@vergil/edit');
   const [config, setConfig] = React.useState(() => parse(rawConfig));
-  const [showJSON, setShowJSON] = React.useState(false);
+  const [pane, setPane] = React.useState('chart');
 
-  let state;
+  let state, action;
 
   try {
     state = getStateFromPath(path.replace(/(^\w+:|^)\/\//, ''), config);
+    action = getActionFromState(state, config);
   } catch (e) {
     // Ignore
   }
@@ -88,35 +107,51 @@ export default function LinkingTester() {
         style={{ ...styles.code, ...styles.editor }}
       />
       <div style={styles.preview}>
-        <button
-          type="button"
-          style={styles.button}
-          onClick={() => setShowJSON((show) => !show)}
-        >
-          {showJSON ? 'Chart' : 'JSON'}
-        </button>
-        {showJSON ? (
-          <Highlight
-            {...defaultProps}
-            code={JSON.stringify(state, null, 2) || ''}
-            theme={theme}
-            language="json"
+        <div style={styles.toggles}>
+          <button
+            type="button"
+            style={styles.button}
+            onClick={() => setPane('chart')}
           >
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-              <pre className={className} style={{ ...style, ...styles.json }}>
-                {tokens.map((line, i) => (
-                  <div {...getLineProps({ line, key: i })}>
-                    {line.map((token, key) => (
-                      <span {...getTokenProps({ token, key })} />
-                    ))}
-                  </div>
-                ))}
-              </pre>
-            )}
-          </Highlight>
-        ) : state ? (
-          <RouteMap routes={state.routes} />
-        ) : <p style={styles.error}>Failed to parse the path. Make sure that the path matches the patterns specified in the config.</p>}
+            Chart
+          </button>
+          <button
+            type="button"
+            style={styles.button}
+            onClick={() => setPane('state')}
+          >
+            State
+          </button>
+          <button
+            type="button"
+            style={styles.button}
+            onClick={() => setPane('action')}
+          >
+            Action
+          </button>
+        </div>
+        {pane === 'state' ? (
+          <Code
+            theme={theme}
+            code={JSON.stringify(state, null, 2) || ''}
+            language="json"
+          />
+        ) : pane === 'action' ? (
+          <Code
+            theme={theme}
+            code={JSON.stringify(action, null, 2) || ''}
+            language="json"
+          />
+        ) : pane === 'chart' ? (
+          state ? (
+            <RouteMap routes={state.routes} />
+          ) : (
+            <p style={styles.error}>
+              Failed to parse the path. Make sure that the path matches the
+              patterns specified in the config.
+            </p>
+          )
+        ) : null}
       </div>
     </>
   );
@@ -145,7 +180,7 @@ const styles = {
     position: 'relative',
     border: '1px solid var(--ifm-contents-border-color)',
     borderRadius: 'var(--ifm-pre-border-radius)',
-    minHeight: 70
+    minHeight: 70,
   },
   json: {
     margin: 0,
@@ -153,18 +188,23 @@ const styles = {
     fontSize: 'var(--ifm-code-font-size)',
     borderRadius: 'var(--ifm-pre-border-radius)',
     padding: 'var(--ifm-pre-padding)',
-    minHeight: 70
+    minHeight: 70,
+  },
+  toggles: {
+    position: 'absolute',
+    flexDirection: 'row',
+    right: 0,
+    top: 0,
+    borderBottom: '1px solid var(--ifm-contents-border-color)',
   },
   button: {
-    position: 'absolute',
-    right: 0,
-    border: '1px solid var(--ifm-contents-border-color)',
-    borderRadius: '0 var(--ifm-pre-border-radius)',
-    borderRight: 0,
-    borderTop: 0,
+    border: 0,
+    borderLeft: '1px solid var(--ifm-contents-border-color)',
+    borderRadius: 0,
     cursor: 'pointer',
-    display: 'block',
+    display: 'inline-flex',
     fontSize: 12,
+    margin: 0,
     padding: '4px 8px',
     color: 'inherit',
     background: 'none',
@@ -173,6 +213,6 @@ const styles = {
   },
   error: {
     margin: 24,
-    color: '#A12027'
-  }
+    color: '#A12027',
+  },
 };
