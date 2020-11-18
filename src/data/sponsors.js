abd9804d6380c9458e52dfc36c093ea561c283ce
Brent Vatne <brentvatne@gmail.com>
2020-11-18 14:31:07 -0800

    Update sponsors

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -40,19 +40,14 @@ export default [
     "name": "Miguel Contreras"
   },
   {
-    "avatarUrl": "https://avatars0.githubusercontent.com/u/3090527?u=6cf70a882423f683b5fa93cf02b43a50b05c482e&v=4",
-    "username": "elie222",
-    "name": "Elie"
-  },
-  {
-    "avatarUrl": "https://avatars1.githubusercontent.com/u/5347038?u=f8284e33e1b7f16b4b7df8b681918dca4f5bdb56&v=4",
-    "username": "Salakar",
-    "name": "Mike Diarmid"
-  },
-  {
     "avatarUrl": "https://avatars2.githubusercontent.com/u/6936373?u=4edd14e6636c45d10ac6a3eecb4b3ffa6cc2bf5c&v=4",
     "username": "Naturalclar",
     "name": "Jesse Katsumata"
+  },
+  {
+    "avatarUrl": "https://avatars1.githubusercontent.com/u/14099522?u=965e74751a9db0f40a30ace9cd0cb1f82eaf1412&v=4",
+    "username": "aCanalez",
+    "name": "Alexi Canales"
   },
   {
     "avatarUrl": "https://avatars3.githubusercontent.com/u/21980965?u=418759528b6cd702bc1204278ea87b97f32f5e3c&v=4",
@@ -63,11 +58,6 @@ export default [
     "avatarUrl": "https://avatars2.githubusercontent.com/u/27461460?u=b5860875e26d33fd70fd210f4ea74f81cdf9d99b&v=4",
     "username": "hyochan",
     "name": "Hyo"
-  },
-  {
-    "avatarUrl": "https://avatars0.githubusercontent.com/u/28190937?u=a28babe1aeab33e348ef168c80125bb83c6b4562&v=4",
-    "username": "jscott22",
-    "name": "Jason Scott"
   },
   {
     "avatarUrl": "https://avatars1.githubusercontent.com/u/65687793?u=85bc8ea9018a174a1adb79a278b8c284c305c5a3&v=4",
