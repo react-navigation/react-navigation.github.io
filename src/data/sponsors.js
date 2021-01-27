2ed4e87b6e87dafc4ebb2280c4f6fdf978f24ff7
Brent Vatne <brentvatne@gmail.com>
2021-01-26 21:47:31 -0800

    Update sponsors

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,66 +1,66 @@
 export default [
   {
-    "avatarUrl": "https://avatars1.githubusercontent.com/u/90494?u=a7ad2e06021c9547dcf8638a6a38ccc93a76b6fe&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/90494?u=a7ad2e06021c9547dcf8638a6a38ccc93a76b6fe&v=4",
     "username": "brentvatne",
     "name": "Brent Vatne"
   },
   {
-    "avatarUrl": "https://avatars1.githubusercontent.com/u/619186?u=2c48df3212e1e73172ea6294cef90777d5da281d&v=4",
-    "username": "brunolemos",
-    "name": "Bruno Lemos"
-  },
-  {
-    "avatarUrl": "https://avatars3.githubusercontent.com/u/916690?v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/916690?v=4",
     "username": "benevbright",
     "name": "Bright Lee"
   },
   {
-    "avatarUrl": "https://avatars3.githubusercontent.com/u/980234?u=59bb4c6ac0a23b53225bb2235b69c72a960ba83f&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/980234?u=59bb4c6ac0a23b53225bb2235b69c72a960ba83f&v=4",
     "username": "simoncar",
     "name": "Simoncar"
   },
   {
-    "avatarUrl": "https://avatars1.githubusercontent.com/u/1257695?u=e142a0ae6314aa8b462e664db429d926b31bf978&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/1257695?u=e142a0ae6314aa8b462e664db429d926b31bf978&v=4",
     "username": "karszawa",
     "name": "Hiroaki KARASAWA"
   },
   {
-    "avatarUrl": "https://avatars2.githubusercontent.com/u/1566403?u=3df07e2ae72e89a3a3509ba6c2f927115b5f38aa&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/1566403?u=3df07e2ae72e89a3a3509ba6c2f927115b5f38aa&v=4",
     "username": "vonovak",
     "name": "Vojtech Novak"
   },
   {
-    "avatarUrl": "https://avatars1.githubusercontent.com/u/1629785?u=12eb94da6070d00fc924761ce06e3a428d01b7e9&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/1629785?u=12eb94da6070d00fc924761ce06e3a428d01b7e9&v=4",
     "username": "JonnyBurger",
     "name": "Jonny Burger"
   },
   {
-    "avatarUrl": "https://avatars0.githubusercontent.com/u/2590799?u=b900982619800752d4359ad03fe98eef81ccf418&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/2590799?u=b900982619800752d4359ad03fe98eef81ccf418&v=4",
     "username": "maikmacs",
     "name": "Miguel Contreras"
   },
   {
-    "avatarUrl": "https://avatars2.githubusercontent.com/u/6936373?u=4edd14e6636c45d10ac6a3eecb4b3ffa6cc2bf5c&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/6936373?u=4edd14e6636c45d10ac6a3eecb4b3ffa6cc2bf5c&v=4",
     "username": "Naturalclar",
     "name": "Jesse Katsumata"
   },
   {
-    "avatarUrl": "https://avatars1.githubusercontent.com/u/14099522?u=965e74751a9db0f40a30ace9cd0cb1f82eaf1412&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/9881700?u=c92e4a0ccc0bff241e50582bce914b179b6d89b6&v=4",
+    "username": "swcarlosrj",
+    "name": "Carlos Rufo"
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/14099522?u=965e74751a9db0f40a30ace9cd0cb1f82eaf1412&v=4",
     "username": "aCanalez",
     "name": "Alexi Canales"
   },
   {
-    "avatarUrl": "https://avatars3.githubusercontent.com/u/21980965?u=418759528b6cd702bc1204278ea87b97f32f5e3c&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/21980965?u=418759528b6cd702bc1204278ea87b97f32f5e3c&v=4",
     "username": "jarvisluong",
     "name": "Jarvis Luong"
   },
   {
-    "avatarUrl": "https://avatars2.githubusercontent.com/u/27461460?u=b5860875e26d33fd70fd210f4ea74f81cdf9d99b&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/27461460?u=b5860875e26d33fd70fd210f4ea74f81cdf9d99b&v=4",
     "username": "hyochan",
     "name": "Hyo"
   },
   {
-    "avatarUrl": "https://avatars1.githubusercontent.com/u/65687793?u=85bc8ea9018a174a1adb79a278b8c284c305c5a3&v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/65687793?u=85bc8ea9018a174a1adb79a278b8c284c305c5a3&v=4",
     "username": "expo-team",
     "name": "Expo team"
   }
