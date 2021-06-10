6ac6b4970d2820dae6eaada3141ae7f2de1cd4be
Brent Vatne <brentvatne@gmail.com>
2021-06-09 21:11:39 -0700

    Update sponsors

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,11 +1,21 @@
 export default [
   {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/9664363?u=78ff34b8adf65d4b93966e6309795e42740beef2&v=4",
+    "username": "EvanBacon",
+    "name": "Evan Bacon"
+  },
+  {
     "avatarUrl": "https://avatars.githubusercontent.com/u/90494?u=a7ad2e06021c9547dcf8638a6a38ccc93a76b6fe&v=4",
     "username": "brentvatne",
     "name": "Brent Vatne"
   },
   {
-    "avatarUrl": "https://avatars.githubusercontent.com/u/916690?v=4",
+    "avatarUrl": "https://avatars.githubusercontent.com/u/306134?v=4",
+    "username": "wcandillon",
+    "name": "William Candillon"
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/916690?u=66482eb2c5bb755553afbcfa219dcacc42fc487a&v=4",
     "username": "benevbright",
     "name": "Bright Lee"
   },
@@ -13,6 +23,11 @@ export default [
     "avatarUrl": "https://avatars.githubusercontent.com/u/980234?u=59bb4c6ac0a23b53225bb2235b69c72a960ba83f&v=4",
     "username": "simoncar",
     "name": "Simoncar"
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/1057756?u=15c3cdff1c715ac27bbc63ccb8f0a1c27eeb3784&v=4",
+    "username": "zhigang1992",
+    "name": "Zhigang Fang"
   },
   {
     "avatarUrl": "https://avatars.githubusercontent.com/u/1257695?u=e142a0ae6314aa8b462e664db429d926b31bf978&v=4",
@@ -30,9 +45,24 @@ export default [
     "name": "Jonny Burger"
   },
   {
-    "avatarUrl": "https://avatars.githubusercontent.com/u/2590799?u=b900982619800752d4359ad03fe98eef81ccf418&v=4",
-    "username": "maikmacs",
-    "name": "Miguel Contreras"
+    "avatarUrl": "https://avatars.githubusercontent.com/u/1764217?u=f36737d852ffcc50b87e809474faa27eb2ce130a&v=4",
+    "username": "markholland",
+    "name": "Mark Holland"
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/1885080?v=4",
+    "username": "coinbase",
+    "name": "Coinbase"
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/5605177?v=4",
+    "username": "Razorholt",
+    "name": null
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/6457344?u=47e100289441b7f4681a7809202ff683886e4f5e&v=4",
+    "username": "ryo-rm",
+    "name": "ryo kishida"
   },
   {
     "avatarUrl": "https://avatars.githubusercontent.com/u/6936373?u=4edd14e6636c45d10ac6a3eecb4b3ffa6cc2bf5c&v=4",
@@ -40,9 +70,14 @@ export default [
     "name": "Jesse Katsumata"
   },
   {
-    "avatarUrl": "https://avatars.githubusercontent.com/u/9881700?u=c92e4a0ccc0bff241e50582bce914b179b6d89b6&v=4",
-    "username": "swcarlosrj",
-    "name": "Carlos Rufo"
+    "avatarUrl": "https://avatars.githubusercontent.com/u/7029942?u=5eb1fed31f05fe97bcb07227268165c4028cc662&v=4",
+    "username": "radko93",
+    "name": "Radek Czemerys"
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/13601619?v=4",
+    "username": "itiden",
+    "name": "Itiden"
   },
   {
     "avatarUrl": "https://avatars.githubusercontent.com/u/14099522?u=965e74751a9db0f40a30ace9cd0cb1f82eaf1412&v=4",
@@ -58,6 +93,16 @@ export default [
     "avatarUrl": "https://avatars.githubusercontent.com/u/27461460?u=b5860875e26d33fd70fd210f4ea74f81cdf9d99b&v=4",
     "username": "hyochan",
     "name": "Hyo"
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/30093692?u=d99abcc5a79ddc4303d11e66a74a5c10142e734c&v=4",
+    "username": "henrymoulton",
+    "name": null
+  },
+  {
+    "avatarUrl": "https://avatars.githubusercontent.com/u/36824170?u=18c3b7d6e7db72426a6d50712eb7b50c67d669b8&v=4",
+    "username": "luism3861",
+    "name": "Luis Medina Huerta"
   },
   {
     "avatarUrl": "https://avatars.githubusercontent.com/u/65687793?u=85bc8ea9018a174a1adb79a278b8c284c305c5a3&v=4",
