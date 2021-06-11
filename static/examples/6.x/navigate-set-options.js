8d6789f8001c3ce431b60d546cea8e4b11b27a82
kacperkapusciak <kapusciak.k@gmail.com>
2021-06-11 15:07:33 +0200

    docs: use native-stack in navigation-prop

--- static/examples/6.x/navigate-set-options.js
+++ static/examples/6.x/navigate-set-options.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen({ navigation: { navigate } }) {
   return (
@@ -36,7 +36,7 @@ function ProfileScreen({ navigation, route }) {
   );
 }
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
