8c4f7bf1f755fb73197d58d9d92bed72a241e8c4
kacperkapusciak <kapusciak.k@gmail.com>
2021-06-14 10:08:04 +0200

    docs: use native-stack in use-navigation

--- static/examples/6.x/use-navigation-example.js
+++ static/examples/6.x/use-navigation-example.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
 
 function MyBackButton() {
@@ -35,7 +35,7 @@ function ProfileScreen() {
   );
 }
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
