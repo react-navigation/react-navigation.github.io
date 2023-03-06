7c8e8e482c3b315f2c3b59d56bfe6004aaf4c17c
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-03-06 14:46:12 +0100

    Emphasize stack nesting

--- static/examples/5.x/multiple-navigators.js
+++ static/examples/5.x/multiple-navigators.js
@@ -36,11 +36,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="EditPost" component={EmptyScreen} />
       </Stack.Navigator>
     </NavigationContainer>
