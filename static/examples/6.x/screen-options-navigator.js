e00ac3f2e69654cb871c163d58c7fec9892c61be
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-03-06 14:04:11 +0100

    Cleanup screen-options-navigator

--- static/examples/6.x/screen-options-navigator.js
+++ static/examples/6.x/screen-options-navigator.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
