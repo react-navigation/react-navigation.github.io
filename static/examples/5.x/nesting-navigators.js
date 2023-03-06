ef879ca7a9765d1dddeb18d5b14f96fa038b5fbf
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-03-06 14:34:52 +0100

    Update nesting-navigators

--- static/examples/5.x/nesting-navigators.js
+++ static/examples/5.x/nesting-navigators.js
@@ -4,8 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 function Feed({ navigation }) {
   return (
--- static/examples/6.x/nesting-navigators.js
+++ static/examples/6.x/nesting-navigators.js
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 function Feed({ navigation }) {
   return (
@@ -41,18 +44,11 @@ function Home() {
   );
 }
 
-const Tab = createMaterialBottomTabNavigator();
-const Stack = createNativeStackNavigator();
-
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
