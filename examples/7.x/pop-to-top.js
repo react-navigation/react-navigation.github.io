86c1a30025b2766a54a006c2e728cdd9266ac18a
satya164 <satyajit.happy@gmail.com>
2023-12-01 11:20:35 +0000

    Deploy website - based on f111a4c729f7583f1aaf538a7ca16ba02c815683

--- examples/7.x/pop-to-top.js
+++ examples/7.x/pop-to-top.js
@@ -23,7 +23,7 @@ function DetailsScreen({ navigation }) {
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
       />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go to Home" onPress={() => navigation.popTo('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
         title="Go back to first screen in stack"
