db62f8af68dfc8eacc715b24512e8241b4a2c130
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-03-08 21:53:21 +0100

    Bump version for bottom tabs

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,5 @@
 import { useColorMode } from '@docusaurus/theme-common';
 import MDXPre from '@theme-original/MDXComponents/Pre';
-import CodeBlock from '@theme-original/CodeBlock';
 import React from 'react';
 
 const peers = {
@@ -10,7 +9,7 @@ const peers = {
 
 const versions = {
   7: {
-    '@react-navigation/bottom-tabs': ['7.0.0-alpha.13', peers],
+    '@react-navigation/bottom-tabs': ['7.0.0-alpha.14', peers],
     '@react-navigation/core': '7.0.0-alpha.11',
     '@react-navigation/native': '7.0.0-alpha.12',
     '@react-navigation/drawer': [
