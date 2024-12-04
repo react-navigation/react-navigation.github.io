3ce61b80754ff9c0db516863f75af405ca3c809a
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-12-04 22:46:00 +0100

    Tweak the home page

--- src/pages/home/Splash/index.js
+++ src/pages/home/Splash/index.js
@@ -5,7 +5,6 @@ import React from 'react';
 import SplashLeftIllustration from './SplashLeftIllustration';
 import SplashRightIllustration from './SplashRightIllustration';
 import styles from './styles.module.css';
-import Spiro from '/img/spiro_header.svg';
 
 export default function Splash() {
   return (
@@ -13,24 +12,22 @@ export default function Splash() {
       <div className={styles.container}>
         <SplashLeftIllustration />
         <div className={styles.main}>
-          <div className={styles.spiroContainer}>
-            <Spiro />
-          </div>
           <div className={styles.mainContent}>
             <h1 className={styles.mainText}>React Navigation</h1>
             <h3 className={styles.subText}>
-              Routing and navigation for Expo and React Native apps.
+              Routing and navigation for React Native and Web apps.
             </h3>
             <div className={styles.buttonContainer}>
               <Link
                 to={useBaseUrl('/docs/getting-started')}
-                className={styles.button}
+                className={`${styles.button} ${styles.buttonPrimary}`}
               >
                 Read Docs
               </Link>
               <a
+                target="_blank"
                 href="https://github.com/react-navigation/react-navigation/tree/main/example"
-                className={`${styles.button} ${styles.buttonPrimary}`}
+                className={`${styles.button}`}
               >
                 Try It
               </a>
