9bde8b2087e84468e11411db7f033e1ca86ffdee
satya164 <satyajit.happy@gmail.com>
2025-03-09 14:29:01 +0000

    Deploy website - based on b827231e1c6a7543c472aa5a5377351f4905fabb

--- assets/js/8ea09047.ed2bcca2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkreact_navigation_website_next=self.webpackChunkreact_navigation_website_next||[]).push([["62494"],{28598:function(e){e.exports=JSON.parse('{"metadata":{"permalink":"/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":17,"previousPage":"/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
